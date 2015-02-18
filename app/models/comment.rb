fb3ce24c1bddf6ceb983e775c9abd2d47fca491f
johannesE <johannes.eifert@unifr.ch>
2015-02-18 19:54:28 +0100

    comments have comments (model only)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,7 +9,18 @@ class Comment
   # property :updated_on, type: Date
 
   has_many :in, :users, unique: true, rel_class: Rating
-  has_one :out, :articles, type: :comment_on
-  has_one :in, :author, unique:true, type: :authored, model_class: User
+  has_one :out, :article, type: :comment_on
+  has_one :in, :author, unique: true, type: :authored, model_class: User
+
+  has_many :both, :comments, model_class: Comment, unique: true
+
+  def get_number_of_replies
+    if comments.count == 0
+      0
+    else
+      comments.count + comments.each.get_number_of_replies
+    end
+  end
+
 
 end
