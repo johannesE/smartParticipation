ef0de97b8b31c730c4264f924e414004eaf87fd0
johannesE <johannes.eifert@unifr.ch>
2015-02-22 21:27:34 +0100

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
 
   config.neo4j.session_type = :server_db
   config.neo4j.session_path = ENV["GRAPHENEDB_URL"] || 'http://localhost:7474'
-  config.action_mailer.default_url_options = { :host => 'smartparticipation.herokuapp.com:80' }
+  config.action_mailer.default_url_options = { :host => 'http://smartparticipation.herokuapp.com:80' }
 end
