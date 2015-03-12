decd631b53ee1c15ed14ce92b48906c54c1c0087
johannesE <johannes.eifert@unifr.ch>
2015-03-12 16:49:35 +0100

    popularity stub

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article
   include Neo4j::ActiveNode
-  before_save :get_standard_deviation_of_ratings
+  before_save :get_standard_deviation_of_ratings, :get_popularity
 
   property :title, type: String
   property :body, type: String
@@ -12,6 +12,8 @@ class Article
   # property :updated_on, type: Date
 
   property :standard_deviation, type: Float
+
+  property :popularity, type: Float
 
   has_many :in, :ratings, unique: true, rel_class: Rating
   has_many :in, :comments, unique: true, type: :comment_on
@@ -47,6 +49,10 @@ class Article
     square_difference.collect{ |s| variance += s}
     variance = variance / get_number_of_ratings(ratings)
     self.standard_deviation = Math.sqrt(variance) # will be returned
+  end
+
+  def get_popularity
+    self.popularity = 0
   end
 
 end
