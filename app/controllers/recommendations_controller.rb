827aaff2bc897cafe5a54a02465ae15d15689527
johannesE <johannes.eifert@unifr.ch>
2015-04-13 16:33:35 +0200

    progress of political party users recommendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -10,12 +10,20 @@ class RecommendationsController < ApplicationController
     user_id = current_user.id
     #  MATCH (n:User) <--> (m) <--> (user:User) WHERE (n <> m AND n.uuid = '8f4e7dd7-d047-427f-8540-7102ef008529') RETURN count(distinct m), user ORDER BY count(distinct m) DESC LIMIT {limit_21} | {"limit_21"=>21}
     result = Neo4j::Session.query.
-        match("(n:User) <--> (m) <--> (user:User)").
-        where("n <> m AND n.uuid = '#{user_id}'").
-        return("count(distinct m), user").
+        match("(me:User) <--> (m) <--> (other:User)").
+        where("me <> m AND me <> other AND me.uuid = '#{user_id}'").
+        return("count(distinct m), other").
         order("count(distinct m) DESC").
-        limit(20)
-    @users = result.to_a.collect{|r| r.user}
+        limit(10)
+    @users = result.to_a.collect{|r| r.other}
+
+    result = Neo4j::Session.query.
+        match("(me:User) -[r1:`rates`]-> (m) <-[r2:`rates`]- (other:User)").
+        where("me <> other AND me.uuid = '#{user_id}'").
+        return("(ABS(r1.value - r2.value)), other"). # abs = absolute value in cypher
+        order("(ABS(r1.value - r2.value)) DESC").
+        limit(10)
+    @political_users = result.to_a.collect{|r| r.other}
 
   end
 end
