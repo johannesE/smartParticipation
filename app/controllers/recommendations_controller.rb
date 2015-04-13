3e709d1e9932c004908e3be5b644287be5abde66
johannesE <johannes.eifert@unifr.ch>
2015-04-13 17:55:46 +0200

    refactoring (code moved, comments added)

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,15 @@
 class RecommendationsController < ApplicationController
   def show
+    # setup
+    user_id = current_user.id
+
     # new users:
     @new_user_articles = Article.all.order(recommender_value: :desc ).limit(20)
-    logger.info @new_user_articles
 
     #the following is only accurate for experienced users.
     @articles = Article.all.limit(20)
 
-    user_id = current_user.id
+    # Users with lots of interaction
     #  MATCH (n:User) <--> (m) <--> (user:User) WHERE (n <> m AND n.uuid = '8f4e7dd7-d047-427f-8540-7102ef008529') RETURN count(distinct m), user ORDER BY count(distinct m) DESC LIMIT {limit_21} | {"limit_21"=>21}
     result = Neo4j::Session.query.
         match("(me:User) <--> (m) <--> (other:User)").
@@ -17,6 +19,7 @@ class RecommendationsController < ApplicationController
         limit(10)
     @users = result.to_a.collect{|r| r.other}
 
+    # users with the same opinion
     result = Neo4j::Session.query.
         match("(me:User) -[r1:`rates`]-> (m) <-[r2:`rates`]- (other:User)").
         where("me <> other AND me.uuid = '#{user_id}'").
