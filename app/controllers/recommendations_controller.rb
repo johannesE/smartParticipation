2f9dbd1cdb181811e8a10209c8a9dbbe722dc622
johannesE <johannes.eifert@unifr.ch>
2015-04-20 17:03:49 +0200

    formatting

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,7 +8,7 @@ class RecommendationsController < ApplicationController
     user_id = current_user.id
 
     # new users:
-    @new_user_articles = Article.all.order(recommender_value: :desc ).limit(20)
+    @new_user_articles = Article.all.order(recommender_value: :desc).limit(20)
 
     #the following is only accurate for experienced users.
 
@@ -24,24 +24,24 @@ class RecommendationsController < ApplicationController
 
     # users with the same opinion (TODO: include the average rating of a user.)
     political_query = Neo4j::Session.query.
-        match("(me:User) -[r1:`rates`]-> (a) <-[r2:`rates`]- (other:User)"). # m can be user or article.
+        match("(me:User) -[r1:`rates`]-> (a) <-[r2:`rates`]- (other:User)").# m can be user or article.
         where("me <> other AND me.uuid = '#{user_id}'").
-        with("(a.standard_deviation - ABS(r1.value - r2.value)) AS rating_diff, other"). # ABS() = absolute value in cypher
-        return("other as user, sum(rating_diff) AS computed"). # other is the group key
-        order("computed ASC"). # The weighted rating difference should be small
+        with("(a.standard_deviation - ABS(r1.value - r2.value)) AS rating_diff, other").# ABS() = absolute value in cypher
+        return("other as user, sum(rating_diff) AS computed").# other is the group key
+        order("computed ASC").# The weighted rating difference should be small
         limit(10)
     @political_users = political_query.to_a
 
-    political_users = @political_users.collect{|po| po.user}
+    political_users = @political_users.collect { |po| po.user }
     other_ids = Array.new
     political_users.each do |u|
       other_ids << u.id # add the user id to the query
     end
     # unread articles from my favourite political users
-    article_query =  Neo4j::Session.query.
+    article_query = Neo4j::Session.query.
         match("(me:User),(article) <-[r2:`rates`]- (other:User)").
         where("NOT (me) -- (article) AND me.uuid = '#{user_id}' AND other.uuid IN #{other_ids}").
-        return("article, percentileCont(r2.value, 0.5) AS computed"). # percentileCont (x, 0.5 is the median)
+        return("article, percentileCont(r2.value, 0.5) AS computed").# percentileCont (x, 0.5 is the median)
         order("computed")
 
     @articles = article_query.to_a
