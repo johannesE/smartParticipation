37e0ed3626978aa08f9fbfe2ecf141ceef8737e0
johannesE <johannes.eifert@unifr.ch>
2015-04-21 18:07:56 +0200

    combinational query for the users

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -34,20 +34,33 @@ class RecommendationsController < ApplicationController
         limit(10)
     @political_users = political_query.to_a
 
+
     political_users = @political_users.collect { |po| po.user }
     other_ids = Array.new
     political_users.each do |u|
       other_ids << u.id # add the user id to the query
     end
+
     # unread articles from my favourite political users
     article_query = Neo4j::Session.query.
         match("(me:User),(article) <-[r2:`rates`]- (other:User)").
         where("NOT (me) -- (article) AND me.uuid = '#{user_id}' AND other.uuid IN #{other_ids}").
         return("article, percentileCont(r2.value, 0.5) AS computed").# percentileCont (x, 0.5 is the median)
         order("computed")
-
     @articles = article_query.to_a
 
+    opinion_factor = current_user.profile.discussion_preference
+    discussion_factor = (100 - opinion_factor) / 100 # /100 because otherwise the opinion is practically useless
+    combination_user_query = Neo4j::Session.query.
+      match("(me:User) <--> (m) <--> (other:User),
+        (me:User) -[r1:`rates`]-> (a) <-[r2:`rates`]- (other:User)").
+      where("me <> m AND me <> other AND me.uuid = '#{user_id}'").
+      with("(a.standard_deviation - ABS(r1.value - r2.value)) AS rating_diff, other, m").
+      return("(#{discussion_factor} * sum(rating_diff) - #{opinion_factor} * count(distinct m)) AS computed, other AS user").
+      order("computed ASC").
+      limit("21")
+
+    @combination_users = combination_user_query.to_a
 
   end
 end
