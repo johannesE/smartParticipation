71d6690fd86e902f6e5988e6bb515de138b69661
johannesE <johannes.eifert@unifr.ch>
2015-05-14 15:25:55 +0200

    seeds.rb refactoring for a new db without tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,24 +28,24 @@ Category.find_or_create_by(name: 'Politicians')
 Category.find_or_create_by(name: 'Poverty')
 Category.find_or_create_by(name: 'Other')
 
-1.times {
-  user1 = User.new
-  user1.email = 'test_email' + rand.to_s + "@mail.com"
-  user1.username = "testuser " + rand.to_s
-  user1.password = 'valid_password'
-  user1.password_confirmation = 'valid_password'
-  user1.save!
-  2.times{
-    article = Article.new(title: "test " + rand.to_s, body: "testBody")
-    article.save!
-    article.author = user1
-    15.times{
-      rating = rand(101)
-      rand_user = Neo4j::Session.query.match("(u:User)").with("u, rand() as r").return("u").order("r").limit(1).to_a.first.first
-      Rating.create(:value => rating,
-                    :from_node => rand_user, :to_node => article)
-    }
-
-  }
-}
+# 1.times {
+#   user1 = User.new
+#   user1.email = 'test_email' + rand.to_s + "@mail.com"
+#   user1.username = "testuser " + rand.to_s
+#   user1.password = 'valid_password'
+#   user1.password_confirmation = 'valid_password'
+#   user1.save!
+#   2.times{
+#     article = Article.new(title: "test " + rand.to_s, body: "testBody")
+#     article.save!
+#     article.author = user1
+#     15.times{
+#       rating = rand(101)
+#       rand_user = Neo4j::Session.query.match("(u:User)").with("u, rand() as r").return("u").order("r").limit(1).to_a.first.first
+#       Rating.create(:value => rating,
+#                     :from_node => rand_user, :to_node => article)
+#     }
+#
+#   }
+# }
 
